ee834bce74406471c9ff563502f8a5089cb8c852
AlberVicky09 <albervicky09@gmail.com>
2020-03-16 20:33:28 +0100

    Paquete de vida

---
+++ CV ATTACK/Assets/HealthPack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPack : MonoBehaviour
+{
+    public GameObject pickupEffect;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PickUp(other);
+        }
+    }
+
+    void PickUp(Collider player)
+    {
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        Health stats = player.GetComponent<Health>();
+        stats.health = 100f;
+
+        Destroy(gameObject);
+    }
+}
