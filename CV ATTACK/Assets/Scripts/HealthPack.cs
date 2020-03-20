ad7b4e4fe36748fc3631108e4da1ef15c98af241
Requena21 <Requena21@users.noreply.github.com>
2020-03-20 18:55:13 +0100

    mapa3

--- CV ATTACK/Assets/Scripts/HealthPack.cs
+++ CV ATTACK/Assets/Scripts/HealthPack.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class HealthPack : MonoBehaviour
 {
     public GameObject pickupEffect;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
